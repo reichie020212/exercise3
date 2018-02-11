a01f9ca455348e13c2d3debb94996a198a28170d
reichie020212 <redickbutay02@gmail.com>
2018-02-11 12:58:05 +0800

    exercise3 v0.5 with add,edit,delete button

--- mylists/urls.py
+++ mylists/urls.py
@@ -8,7 +8,13 @@ urlpatterns = [
 	url(r'^car/(?P<pk>\d+)/$',views.displaylist,name="displaylist1"),
 	url(r'^manufacturer/(?P<pk>\d+)/$',views.displaylist,name="displaylist2"),
 	url(r'^person/(?P<pk>\d+)/$',views.displaylist,name="displaylist3"),
+	url(r'^car/(?P<pk>\d+)/edit/$',views.edit,name="edit"),
+	url(r'^manufacturer/(?P<pk>\d+)/edit/$',views.edit,name="edit"),
+	url(r'^person/(?P<pk>\d+)/edit/$',views.edit,name="edit"),
 	url(r'^car/$',views.viewCar, name="viewCar"),
 	url(r'^manufacturer/$',views.viewManufacturer,name="viewManufacturer"),
 	url(r'^person/$',views.viewPerson,name="viewPerson"),
+	url(r'^car/(?P<pk>\d+)/delete/', views.delete, name="delete"),
+	url(r'^manufacturer/(?P<pk>\d+)/delete/', views.delete, name="delete"),
+	url(r'^person/(?P<pk>\d+)/delete/', views.delete, name="delete"),
 ]
--- mylists/views.py
+++ mylists/views.py
@@ -61,9 +61,60 @@ def displaylist(request, pk):
 	if "car" in request.META.get("HTTP_REFERER"):
 		car = get_object_or_404(Car, pk=pk)
 		return render(request, 'mylists/displaylist.html', {'car':car})
-	if "manufacturer" in request.META.get("HTTP_REFERER"):
+	elif "manufacturer" in request.META.get("HTTP_REFERER"):
 		manufacturer = get_object_or_404(Manufacturer, pk=pk)
 		return render(request, 'mylists/displaylist.html', {'manufacturer':manufacturer})
-	if "person" in request.META.get("HTTP_REFERER"):
+	elif "person" in request.META.get("HTTP_REFERER"):
 		person = get_object_or_404(Person, pk=pk)
 		return render(request, 'mylists/displaylist.html', {'person':person})
+
+def edit(request,pk):
+	if "car" in request.META.get("HTTP_REFERER"):
+		car = get_object_or_404(Car,pk=pk)
+		if request.method == "POST":
+			form = CarForm(request.POST, instance=car)
+			if form.is_valid():
+				car=form.save()
+				car.save()
+				return redirect('viewCar')
+		else:
+			form = CarForm(instance=car)
+			return render(request,'mylists/add.html',{'form':form})
+	
+	elif "manufacturer" in request.META.get("HTTP_REFERER"):
+		manufacturer = get_object_or_404(Manufacturer,pk=pk)
+		if request.method == "POST":
+			form = ManufacturerForm(request.POST, instance=manufacturer)
+			if form.is_valid():
+				manufacturer=form.save()
+				manufacturer.save()
+				return redirect('viewManufacturer')
+		else:
+			form = ManufacturerForm(instance=manufacturer)
+			return render(request,'mylists/add.html',{'form':form})
+
+	elif "person" in request.META.get("HTTP_REFERER"):
+		person = get_object_or_404(Person,pk=pk)
+		if request.method == "POST":
+			form = PersonForm(request.POST, instance=person)
+			if form.is_valid():
+				person=form.save()
+				person.save()
+				return redirect('viewPerson')
+		else:
+			form = PersonForm(instance=person)
+			return render(request,'mylists/add.html',{'form':form})
+
+def delete(request,pk):
+	if "car" in request.META.get("HTTP_REFERER"):
+		car = get_object_or_404(Car,pk=pk)
+		car.delete()
+		return redirect('viewCar')
+	elif "manufacturer" in request.META.get("HTTP_REFERER"):
+		manufacturer = get_object_or_404(Manufacturer,pk=pk)
+		manufacturer.delete()
+		return redirect('viewManufacturer')
+	elif "person" in request.META.get("HTTP_REFERER"):
+		person = get_object_or_404(Person,pk=pk)
+		person.delete()
+		return redirect('viewPerson')
